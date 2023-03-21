49b466d96d1434c5ee4783cf97d583d22a029341
eweibell <54504318+eweibell@users.noreply.github.com>
2023-03-21 12:07:15 +0100

    import from default

--- pause.py
+++ pause.py
@@ -1,30 +1,9 @@
 import pygame
+from default import my_font, my_fontHeading, white, black, red, green, bright_green, bright_red, display_width, display_height, display, game_over, play_result, x1, y1, clock
 
 pygame.init()
 pygame.font.init()
-
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
-my_fontHeading = pygame.font.SysFont('Comic Sans MS', 50) 
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 155, 0)
-bright_green = (0, 255, 0)
-bright_red = (238, 75, 43)
-
-display_width = 800
-display_height = 600
-
-display = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Menu')
- 
-game_over = False
-play_result = ''
- 
-x1 = display_width/2
-y1 = display_height/2
-
-clock = pygame.time.Clock()
 
 def pause():
         paused = True
